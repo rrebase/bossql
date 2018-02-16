75dcb3ffe4de679b300421f9e3a11eec54d0f170
Ragnar Rebase <rrebase@gmail.com>
2018-02-16 13:04:42 +0200

    Added Tutorials index page, css files are now included using django template blocks

--- bossql/urls.py
+++ bossql/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("tutorials/", include('tutorials.urls')),
     path("challenges/", include('challenges.urls')),
     path("admin/", admin.site.urls),
 ]
