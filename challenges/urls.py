163cbf8388387d34a9e949d0fee94528fe494466
Ragnar Rebase <rrebase@gmail.com>
2018-02-19 22:35:22 +0200

    Challenge view -> generic.DetailView

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+
 app_name = 'challenges'
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:challenge_id>/", views.detail, name="detail"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
 ]
