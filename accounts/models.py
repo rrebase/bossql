1499cb0f34725772f6c711ec18718a92856dc978
Ragnar Rebase <rrebase@gmail.com>
2018-02-23 00:22:23 +0200

    Using a custom user model now, initial default django login and registration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+    pass
+
+
+class CustomUser(AbstractUser):
+    objects = CustomUserManager
+    completed_challenges = models.IntegerField(default=0)
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm
 
+AUTH_USER_MODEL = "accounts.CustomUser"
 
 def login(request):
     return render(request, "accounts/login.html", {})
+
+
+class Register(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("home")
+    template_name = "accounts/register.html"
 
 
 def register(request):
