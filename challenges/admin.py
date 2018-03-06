d69cd8815e8c0a1118991b849c2db964a1b05dc4
Ragnar Rebase <rrebase@gmail.com>
2018-03-06 14:29:59 +0200

    Add challenges to admin

--- challenges/admin.py
+++ challenges/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Challenge
+from .models import Challenge, ChallengeTopic, ChallengeSourceTable, ChallengeResultTable
 
 admin.site.register(Challenge)
-
+admin.site.register(ChallengeTopic)
+admin.site.register(ChallengeSourceTable)
+admin.site.register(ChallengeResultTable)
--- challenges/utils.py
+++ challenges/utils.py
@@ -1,4 +1,4 @@
 import psycopg2
 
 def get_env_db_conn():
-    return psycopg2.connect("dbname=bossql_env user=postgres password=54321")
+    return psycopg2.connect("dbname=bossql_env user=rareba password=UnrealhfK9")
