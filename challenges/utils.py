0fe0fcb444556672bfb39cf2cd08bc4332165887
Magnus Teekivi <magnus.teekivi@gmail.com>
2018-03-13 10:49:56 +0200

    Make env db connect use settings instead of hardcoded credentials.
    Also added db.sqlite3 to .gitignore and made local_settings to be
    imported from the current package.

--- challenges/utils.py
+++ challenges/utils.py
@@ -1,4 +1,6 @@
 import psycopg2
 
+from django.conf import settings
+
 def get_env_db_conn():
-    return psycopg2.connect("dbname=bossql_env user=rareba password=***REMOVED***")
+    return psycopg2.connect(**settings.ENV_DATABASE)
